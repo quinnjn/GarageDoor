0025fd102f4b494a9a67c78635c83cee678a52ee
Quinn J Neumiiller <quinn@neumiiller.com>
2014-06-22 17:09:09 -0600

    Adding (/open|/close) endpoints
    These endpoints will control open/close functionality to the garage door module.

--- garagedoor/server.py
+++ garagedoor/server.py
@@ -1,1 +1,19 @@
-#Empty
+from flask import Flask
+
+from garagedoor import GarageDoor
+
+app = Flask(__name__)
+garageDoor = GarageDoor()
+
+@app.route("/open")
+def openGarage():
+	garageDoor.open()
+	return "open"
+
+@app.route("/close")
+def closeGarage():
+	garageDoor.close()
+	return "close"
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=80, debug=True)
