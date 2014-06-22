1146ebbc8ac194e5603f9469539514251037b5a7
Quinn J Neumiiller <quinn@neumiiller.com>
2014-06-22 17:08:27 -0600

    Adding class GarageDoor
    GarageDoor contains simple operations for the pi

--- garagedoor/garagedoor.py
+++ garagedoor/garagedoor.py
@@ -1,1 +1,33 @@
-#Empty
+import time
+
+import RPi.GPIO as GPIO
+
+class GarageDoor:
+	RELAY1 = 18
+	ON = 1
+	OFF = 0
+
+	def __init__(self):
+		self.__setup()
+		print "GarageDoor:Init"
+
+	def __setup(self):
+		GPIO.cleanup()
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(self.RELAY1, GPIO.OUT)
+
+	def __del__(self):
+		GPIO.cleanup()
+
+	def open(self):
+		self.__action()
+		print "GarageDoor::Open"
+
+	def close(self):
+		self.__action()
+		print "GarageDoor::Close"
+
+	def __action(self):
+		GPIO.output(self.RELAY1, self.ON)
+		time.sleep(1)
+		GPIO.output(self.RELAY1, self.OFF)
